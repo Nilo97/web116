da1a15b5d76f071dc8898c54b51868ad083d72ab
Nilo Eugenio Nhalia <57400679+Nilo97@users.noreply.github.com>
2025-10-22 21:13:27 +0200

    Actualizacao das rotas
    Actualizacoes das rotas

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,19 @@ Route::get('/login', function () {
     return view('login'); // se estiver em resources/views/login.blade.php
 });
 
+Route::get('/casos', function () {
+    return view('casos'); // 
+});
+
+
+Route::get('/file-manager', function () {
+    return view('file-manager'); // s
+});
+
+
+Route::get('/formDenuncia', function () {
+    return view('formDenuncia'); // s
+});
+
+
 
