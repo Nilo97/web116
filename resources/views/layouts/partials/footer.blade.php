c513906891193f16fd16039aaddc7fa84bcba9ca
Nilo Eugenio Nhalia <57400679+Nilo97@users.noreply.github.com>
2025-12-15 11:55:56 +0200

    footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,11 +2,11 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-6 footer-copyright">
-        <p class="mb-0">Copyright © 2025 LFC - Linha Fala Criança. Todos os direitos reservados.</p>
+        <p class="mb-0">Copyright © 2025 LFC - Linha Fala Criança. Todos os Direitos Reservados.</p>
       </div>
       <div class="col-md-6">
         <p class="pull-right mb-0">
-          Desenvolvido com dedicação pela Equipa LFC 
+          Desenvolvido pela Equipa LFC 
           <i class="fa fa-heart font-secondary"></i>
         </p>
       </div>
